a3b0c58e888a393d480e64a91611b380b255a5bc
Bipul <Bipulkumarbk@gmail.com>
2020-09-20 20:56:12 +0530

    commit rename repo

---
+++ RenameFile/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace RenameFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExcelName = new System.Windows.Forms.TextBox();
+            this.SelectExcel = new System.Windows.Forms.Button();
+            this.RenameFile = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SelectFolder = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Filecount = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ExcelName
+            // 
+            this.ExcelName.Location = new System.Drawing.Point(12, 42);
+            this.ExcelName.Name = "ExcelName";
+            this.ExcelName.ReadOnly = true;
+            this.ExcelName.Size = new System.Drawing.Size(202, 20);
+            this.ExcelName.TabIndex = 0;
+            // 
+            // SelectExcel
+            // 
+            this.SelectExcel.Location = new System.Drawing.Point(236, 42);
+            this.SelectExcel.Name = "SelectExcel";
+            this.SelectExcel.Size = new System.Drawing.Size(142, 23);
+            this.SelectExcel.TabIndex = 1;
+            this.SelectExcel.Text = "Select CSV File";
+            this.SelectExcel.UseVisualStyleBackColor = true;
+            this.SelectExcel.Click += new System.EventHandler(this.SelectExcel_Click);
+            // 
+            // RenameFile
+            // 
+            this.RenameFile.Location = new System.Drawing.Point(90, 300);
+            this.RenameFile.Name = "RenameFile";
+            this.RenameFile.Size = new System.Drawing.Size(177, 23);
+            this.RenameFile.TabIndex = 2;
+            this.RenameFile.Text = "Rename File";
+            this.RenameFile.UseVisualStyleBackColor = true;
+            this.RenameFile.Click += new System.EventHandler(this.RenameFile_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(418, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(370, 391);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(202, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // SelectFolder
+            // 
+            this.SelectFolder.Location = new System.Drawing.Point(236, 87);
+            this.SelectFolder.Name = "SelectFolder";
+            this.SelectFolder.Size = new System.Drawing.Size(142, 23);
+            this.SelectFolder.TabIndex = 5;
+            this.SelectFolder.Text = "Select Folder for File Rename";
+            this.SelectFolder.UseVisualStyleBackColor = true;
+            this.SelectFolder.Click += new System.EventHandler(this.SelectFolder_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(13, 138);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(372, 150);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // Filecount
+            // 
+            this.Filecount.AutoSize = true;
+            this.Filecount.Location = new System.Drawing.Point(13, 117);
+            this.Filecount.Name = "Filecount";
+            this.Filecount.Size = new System.Drawing.Size(0, 13);
+            this.Filecount.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 360);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(369, 10);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Filecount);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.SelectFolder);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.RenameFile);
+            this.Controls.Add(this.SelectExcel);
+            this.Controls.Add(this.ExcelName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox ExcelName;
+        private System.Windows.Forms.Button SelectExcel;
+        private System.Windows.Forms.Button RenameFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SelectFolder;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label Filecount;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ RenameFile/Form1.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Access;
+
+namespace RenameFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+              
+
+        private void RenameFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(ExcelName.Text) && !string.IsNullOrEmpty(textBox1.Text))
+                {
+                    DataTable dtCSV = new DataTable();
+                    dtCSV = GetDataTabletFromCSVFile(ExcelName.Text);
+
+                    progressBar1.Maximum = dtCSV.Rows.Count;
+                    progressBar1.Step = 1;
+
+                    string folderPath = textBox1.Text + "\\";
+                    for (int i = 0; i < dtCSV.Rows.Count; i++)
+                    {
+                       
+                        progressBar1.PerformStep();
+                        string oldfilename = folderPath + dtCSV.Rows[i]["OldFileName"].ToString();
+                        string newfilename = folderPath + dtCSV.Rows[i]["NewFileName"].ToString();
+                        try
+                        {
+                            System.IO.File.Move(oldfilename, newfilename);
+                        }
+                        catch (Exception ex)
+                        {
+                            
+                        }
+                        
+                    }
+                    MessageBox.Show("File rename completed.");
+                }
+                else
+                {
+                    MessageBox.Show("Please select CSV file and folder for file to rename.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }           
+            
+        }
+
+        private void SelectExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filePath = string.Empty;
+                string fileExt = string.Empty;
+                OpenFileDialog file = new OpenFileDialog(); //open dialog to choose file  
+                if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK) //if there is a file choosen by the user  
+                {
+                    ExcelName.Text = file.FileName;
+                    filePath = file.FileName; //get the path of the file  
+                    fileExt = Path.GetExtension(filePath); //get the file extension  
+                    if (fileExt.CompareTo(".csv") == 0)
+                    {
+                        try
+                        {
+                            DataTable dtExcel = new DataTable();
+                            dtExcel = GetDataTabletFromCSVFile(filePath); //read file  
+                            dataGridView1.Visible = true;
+                            dataGridView1.DataSource = dtExcel;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message.ToString());
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please choose .csv file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); //custom messageBox to show error  
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }         
+
+        }
+
+        public DataTable GetDataTabletFromCSVFile(string csv_file_path)
+        {
+            DataTable csvData = new DataTable();
+
+            try
+            {
+
+                using (TextFieldParser csvReader = new TextFieldParser(csv_file_path))
+                {
+                    csvReader.SetDelimiters(new string[] { "," });
+                    csvReader.HasFieldsEnclosedInQuotes = true;
+                    string[] colFields = csvReader.ReadFields();
+                    foreach (string column in colFields)
+                    {
+                        DataColumn datecolumn = new DataColumn(column);
+                        datecolumn.AllowDBNull = true;
+                        csvData.Columns.Add(datecolumn);
+                    }
+
+                    while (!csvReader.EndOfData)
+                    {
+                        string[] fieldData = csvReader.ReadFields();
+                        //Making empty value as null
+                        for (int i = 0; i < fieldData.Length; i++)
+                        {
+                            if (fieldData[i] == "")
+                            {
+                                fieldData[i] = null;
+                            }
+                        }
+                        csvData.Rows.Add(fieldData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            return csvData;
+        }
+
+        private void SelectFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        textBox1.Text = fbd.SelectedPath.ToString();
+                        string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                        // System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+
+                        Filecount.Text = "Files found: " + files.Length.ToString();
+
+                        DataTable tbl = new DataTable();
+                        tbl.Columns.Add("FileName");
+
+                        foreach (string s in files)
+                        {
+                            DataRow row = tbl.NewRow();
+                            // string[] numb = s.Split(',');
+                            row["FileName"] = s.Replace(fbd.SelectedPath.ToString(), "").Replace("\\", "");
+
+                            tbl.Rows.Add(row);
+                        }
+
+                        dataGridView2.Visible = true;
+                        dataGridView2.DataSource = tbl;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+           
+        }
+    }
+}
